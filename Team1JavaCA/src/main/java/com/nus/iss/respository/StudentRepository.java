d29a2c080532921b513acfa8cc88122893309e4d
Adam Png <adampngys@gmail.com>
2018-12-10 15:45:30 +0800

    Update web project path

--- Team1JavaCA/src/main/java/com/nus/iss/respository/StudentRepository.java
+++ Team1JavaCA/src/main/java/com/nus/iss/respository/StudentRepository.java
@@ -1,9 +1,9 @@
 package com.nus.iss.respository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nus.iss.model.Student;
 
-public interface StudentRepository extends CrudRepository<Student, Integer> {
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 
 }
